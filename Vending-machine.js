b307259877fc7b45d94334759290f67a9ce01242
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-05-24 19:51:50 -0700

    Update Vending-machine.js

--- Vending-machine.js
+++ Vending-machine.js
@@ -32,3 +32,46 @@ Rules:
 
 
 // Solution 
+
+// Transforms dollars to cents as int
+const toCents = dollars => Math.round(dollars * 100)
+
+//Transforms cents to dollars as float
+const toDollars = cents => Math.floor(cents) / 100
+
+class VendingMachine {
+  // Creates a new instance of VendingMachine
+  constructor(items, initialDollars) {
+    this.items = items
+    this.bank = toCents(initialDollars)
+  }
+
+  // Handles vending transaction given a selection and some money
+  vend (selection, cash) {
+    const code = selection.toUpperCase()
+    const cents = toCents(cash)
+    const item = this.items.find(item => item.code === code)
+
+    // handle invalid item
+    if (!item) return `Invalid selection! : Money in vending machine = ${toDollars(this.bank).toFixed(2)}`
+
+    const price = toCents(item.price);
+    
+    // handle insufficient change
+    if (price > cents) return 'Not enough money!'
+
+    // handle insufficient stock
+    if (item.quantity < 1) return `${item.name}: Out of stock!`
+
+    // ready to vend, update internal state
+    const change = cents - price;
+    this.bank += price
+    item.quantity -= 1
+
+    // handle vending with change
+    if (change > 0) return `Vending ${item.name} with ${toDollars(change).toFixed(2)} change.`
+
+    // handle vending with no change
+    return `Vending ${item.name}`
+  }
+}
